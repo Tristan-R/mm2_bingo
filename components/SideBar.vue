115f0c4defbce221173531d7433d7440b432492e
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2020-10-10 16:20:36 +0100

    Update components.

---
+++ components/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <p class="menu-label is-hidden-touch">
+            General
+        </p>
+        <ul class="menu-list">
+            <li
+                v-for="(item, key) of items"
+                :key="key"
+            >
+                <nuxt-link
+                    :to="item.to"
+                    exact-active-class="is-active"
+                >
+                    <b-icon :icon="item.icon" /> {{ item.title }}
+                </nuxt-link>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "SideBar",
+        props: ['items']
+    }
+</script>
+
+<style scoped>
+
+</style>
