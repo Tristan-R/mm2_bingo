5a20e4f4b308d174a6cd84b99f6c036677e35f46
Tristan Russell <39279889+Tristan-R@users.noreply.github.com>
2020-10-11 20:27:57 +0100

    Update home page and add mdi.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,33 @@
 <template>
     <section class="section">
-        <Game></Game>
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <b-field>
+                    <b-button
+                        tag="router-link"
+                        to="/singleplayer"
+                        type="is-link"
+                        expanded>
+                        Single Player
+                    </b-button>
+                </b-field>
+                <b-field>
+                    <b-button
+                        tag="router-link"
+                        to="/multiplayer"
+                        type="is-link"
+                        expanded
+                        disabled>
+                        <b-tooltip
+                            label="I'm working on it"
+                            type="is-dark"
+                            position="is-right">
+                        </b-tooltip>
+                        Multiplayer
+                    </b-button>
+                </b-field>
+            </div>
+        </div>
     </section>
 </template>
 
